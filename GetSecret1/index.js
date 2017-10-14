fd04be5bc9c3f1d39c3ff41eb55d6e932cdb3477
Martin Peck <martinpeck@Martins-MacBook-Pro.local>
2017-10-14 15:06:48 +0100

    get basics in place

--- GetSecret1/index.js
+++ GetSecret1/index.js
@@ -1,23 +1,66 @@
 const rp = require('request-promise');
-const getToken = function(resource, apiver, cb) {
+
+// This function makes a request to teh MSI_ENDPOINT (supplied by the Azure Functions 
+// runtime for any Function App with a managed identity) to obtain an access token for 
+// the Azure Vault resource, and returns a promise
+const getADTokenForVaultAsync = () => {
+    
+    const vaultResourceUrl = 'https://vault.azure.net/';
+    const apiVersion = '2017-09-01';
+
     var options = {
-        uri: `${process.env["MSI_ENDPOINT"]}/?resource=${resource}&api-version=${apiver}`,
+        uri: `${process.env["MSI_ENDPOINT"]}/?resource=${vaultResourceUrl}&api-version=${apiVersion}`,
         headers: {
             'Secret': process.env["MSI_SECRET"]
         }
     };
-    rp(options)
-        .then(cb);
+
+    return rp(options);
+}
+
+// given an access token and a secretUrl, this function returns the value of the secret
+const getSecretFromVaultAsync = (token, secretUrl) => {
+
+    var options = {
+        url: secretUrl,
+        headers : {
+            'Authorization' : `Bearer ${token}`
+        }
+    };
+
+    return rp(options);
+}
+
+const logDiagnostics = (context) => {
+    context.log(`MSI_ENDPOINT: ${process.env.MSI_ENDPOINT}`);
+    context.log(`MSI_SECRET: ${process.env.MSI_SECRET}`);
+    context.log(`SECRET1: ${process.env.SECRET1}`);
 }
 
 module.exports = function (context, req) {
-    context.log('GetSecret1 triggered');
-
     
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: "Hello!!"
-    };
+    logDiagnostics(context);
 
-    context.done();
+    const secretUrl = process.env["SECRET1"];
+    context.log(`Accessing secretUrl:${secretUrl}`);
+
+    getADTokenForVaultAsync()
+        .then( token => {
+            context.log(token);
+
+            getSecretFromVaultAsync(token, secretUrl)
+            .then( resp => {
+                context.log(response)
+                context.res = response
+                context.done()
+            })
+            .catch( err => {
+                context.log(err);
+                context.done("Unable to access secret")
+            })
+        })
+        .catch( err => {
+            context.log(err);
+            context.done("Unable to obtain AD token to access Key Vault");
+        })
 };
